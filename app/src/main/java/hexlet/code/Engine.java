525bb8998873510232e938a1dadf482b5ef5aae6
Artur Akhmetov <artur.akh@internet.ru>
2024-05-13 18:57:33 +0300

    Add Engine and Makefile to git

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,35 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+    public static void main(String[] args) {
+    }
+    public static final int ROUNDS = 3;
+
+    public static void playGame(String question, String[][] questionAndAnswer) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        System.out.println(question);
+
+        for (var i = 0; i < ROUNDS; i++) {
+            Scanner scannerAnswer = new Scanner(System.in);
+
+            System.out.println("Question :" + questionAndAnswer[i][0]);
+            String answer = scannerAnswer.next();
+            if (answer.toLowerCase().equals(questionAndAnswer[i][1])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("' " + answer + "'" + " is wrong answer ;(. Correct answer was '"
+                        + questionAndAnswer[i][1] + "'\n Lets try again, " + userName);
+            }
+
+        }
+
+        System.out.println("Congratulations, " + userName + "!");
+    }
+}
